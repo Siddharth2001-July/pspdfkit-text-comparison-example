6b7fca13f770b090bbdf0c6bb0e9fe432d5f83f5
Jon Addams <jonaddams@gmail.com>
2024-07-12 13:33:24 -0400

    remove TextChanges import statement

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,6 @@
 "use client";
 
 import {useEffect, useRef, useState} from "react";
-import TextChanges from "./TextChanges";
 
 export default function Home() {
   const originalContainerRef = useRef(null);
