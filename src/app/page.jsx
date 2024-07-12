9cec7f8425d93d0186d00d86d10331bc6c2a2cbf
Jon Addams <jonaddams@Jons-MacBook-Pro.local>
2024-07-12 13:19:14 -0400

    Scroll synchronization, remove TextChanges, revise Changes sidebar

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,18 +1,16 @@
 "use client";
 
-import { useEffect, useRef, useState } from "react";
+import {useEffect, useRef, useState} from "react";
 import TextChanges from "./TextChanges";
 
 export default function Home() {
-  console.log("rendering Home");
-
   const originalContainerRef = useRef(null);
   const changedContainerRef = useRef(null);
   const operationsRef = useRef(new Map());
 
   const [operationsMap, setOperationsMap] = useState(new Map());
+
   function updateOperationsMap(existingMap) {
-    console.log("updateOperationsMap");
     setOperationsMap(new Map(existingMap));
   }
 
@@ -21,11 +19,10 @@ export default function Home() {
   const numberOfContextWords = 100;
 
   useEffect(() => {
-    console.log("useEffect");
     const originalContainer = originalContainerRef.current;
     const changedContainer = changedContainerRef.current;
-    const deleteHighlightColor = { r: 255, g: 201, b: 203 }; // #FFC9CB
-    const insertHighlightColor = { r: 192, g: 216, b: 239 }; // #C0D8EF
+    const deleteHighlightColor = {r: 255, g: 201, b: 203}; // #FFC9CB
+    const insertHighlightColor = {r: 192, g: 216, b: 239}; // #C0D8EF
 
     if (typeof window !== "undefined") {
       import("pspdfkit").then(async (PSPDFKit) => {
@@ -37,6 +34,7 @@ export default function Home() {
           container: originalContainer,
           document: originalDoc,
           baseUrl: `${window.location.protocol}//${window.location.host}/`,
+          styleSheets: ["/styles.css"],
         });
 
         let changedInstance = await PSPDFKit.load({
@@ -45,10 +43,37 @@ export default function Home() {
           baseUrl: `${window.location.protocol}//${window.location.host}/`,
         });
 
+        // add event listeners to sync the view state to the right viewer
+        const scrollElement = changedInstance.contentDocument.querySelector(".PSPDFKit-Scroll");
+        scrollElement.addEventListener("scroll", syncViewState);
+        changedInstance.addEventListener("viewState.currentPageIndex.change", syncViewState);
+        changedInstance.addEventListener("viewState.zoom.change", syncViewState);
+
+        // synchronize the view state of the original instance viewer to the changed instance viewer
+        function syncViewState() {
+          // Get the current view state from the left viewer
+          const customViewState = {
+            pageNumber: changedInstance.viewState.currentPageIndex,
+            zoomLevel: changedInstance.viewState.zoom,
+            scrollLeft: changedInstance.contentDocument.querySelector(".PSPDFKit-Scroll").scrollLeft,
+            scrollTop: changedInstance.contentDocument.querySelector(".PSPDFKit-Scroll").scrollTop,
+          };
+
+          // Set the page number and zoom level for the right viewer
+          let viewState = originalInstance.viewState;
+          originalInstance.setViewState(viewState.set("currentPageIndex", customViewState.pageNumber));
+          originalInstance.setViewState(viewState.set("zoom", customViewState.zoomLevel));
+
+          // Set scroll position for the right viewer
+          const scrollElement = originalInstance.contentDocument.querySelector(".PSPDFKit-Scroll");
+          scrollElement.scrollLeft = customViewState.scrollLeft;
+          scrollElement.scrollTop = customViewState.scrollTop;
+        }
+
         let totalPageCount = await originalInstance.totalPageCount;
 
         for (let i = 0; i < totalPageCount; i++) {
-          console.log("page: ", i + 1, " of ", totalPageCount);
+          console.log("comparing page: ", i + 1, " of ", totalPageCount);
 
           const originalDocument = new PSPDFKit.DocumentDescriptor({
             filePath: originalDoc,
@@ -60,17 +85,11 @@ export default function Home() {
             pageIndexes: [i],
           });
 
-          const textComparisonOperation = new PSPDFKit.ComparisonOperation(
-            PSPDFKit.ComparisonOperationType.TEXT,
-            {
-              numberOfContextWords: numberOfContextWords,
-            }
-          );
+          const textComparisonOperation = new PSPDFKit.ComparisonOperation(PSPDFKit.ComparisonOperationType.TEXT, {
+            numberOfContextWords: numberOfContextWords,
+          });
 
-          const comparisonResult = await originalInstance.compareDocuments(
-            { originalDocument, changedDocument },
-            textComparisonOperation
-          );
+          const comparisonResult = await originalInstance.compareDocuments({originalDocument, changedDocument}, textComparisonOperation);
 
           let originalInstanceRects = PSPDFKit.Immutable.List([
             // new PSPDFKit.Geometry.Rect({
@@ -92,10 +111,8 @@ export default function Home() {
             for (let k = 0; k < comparisonResult[j].documentComparisonResults.length; k++) {
               for (let l = 0; l < comparisonResult[j].documentComparisonResults[k].comparisonResults.length; l++) {
                 for (let m = 0; m < comparisonResult[j].documentComparisonResults[k].comparisonResults[l].hunks.length; m++) {
-                  // console.log("hunk: ", comparisonResult[j].documentComparisonResults[k].comparisonResults[l].hunks[m]);
                   for (let n = 0; n < comparisonResult[j].documentComparisonResults[k].comparisonResults[l].hunks[m].operations.length; n++) {
                     let operation = comparisonResult[j].documentComparisonResults[k].comparisonResults[l].hunks[m].operations[n];
-                    // console.log("operation: ", operation);
                     switch (operation.type) {
                       case "equal":
                         break;
@@ -165,32 +182,25 @@ export default function Home() {
             }
           } // end of for comparisonResult iteration
 
-          operationsRef.current = new Map([
-            ...operationsRef.current,
-            ...changes,
-          ]);
+          operationsRef.current = new Map([...operationsRef.current, ...changes]);
 
-          let originalAnnotations =
-            new PSPDFKit.Annotations.HighlightAnnotation({
-              pageIndex: i,
-              rects: originalInstanceRects,
-              color: new PSPDFKit.Color(deleteHighlightColor),
-            });
+          let originalAnnotations = new PSPDFKit.Annotations.HighlightAnnotation({
+            pageIndex: i,
+            rects: originalInstanceRects,
+            color: new PSPDFKit.Color(deleteHighlightColor),
+          });
 
-          let changedAnnotations = new PSPDFKit.Annotations.HighlightAnnotation(
-            {
-              pageIndex: i,
-              rects: changedInstanceRects,
-              color: new PSPDFKit.Color(insertHighlightColor),
-            }
-          );
+          let changedAnnotations = new PSPDFKit.Annotations.HighlightAnnotation({
+            pageIndex: i,
+            rects: changedInstanceRects,
+            color: new PSPDFKit.Color(insertHighlightColor),
+          });
 
           await originalInstance.create(originalAnnotations);
           await changedInstance.create(changedAnnotations);
         } // end of for totalPageCount iteration
 
         // update state, which should trigger re-render
-        console.log("calling updateOperationsMap");
         updateOperationsMap(operationsRef.current);
       });
     }
@@ -199,20 +209,19 @@ export default function Home() {
   function plusMinusDisplayText(operation) {
     if (operation.insert && operation.del) {
       return (
-        <div>
-          <span className="bg-[#FFC9CB]">-1</span> |{" "}
-          <span className="bg-[#C0D8EF]">+1</span>
+        <div className="text-xs">
+          <span className="bg-[#FFC9CB]">-1</span> | <span className="bg-[#C0D8EF]">+1</span>
         </div>
       );
     } else if (operation.insert) {
       return (
-        <div>
+        <div className="text-xs">
           <span className="bg-[#C0D8EF]">+1</span>
         </div>
       );
     } else {
       return (
-        <div>
+        <div className="text-xs">
           <span className="bg-[#FFC9CB]">-1</span>
         </div>
       );
@@ -226,38 +235,34 @@ export default function Home() {
           <div>
             <p className="text-center p-3">{originalDoc}</p>
           </div>
-          <div ref={originalContainerRef} style={{ height: "100vh" }} />
+          <div id="original-document-viewer" ref={originalContainerRef} className="h-lvh" />
         </div>
         <div className="min-h-[100px] col-span-5 border-1">
           <div>
             <p className="text-center p-3">{changedDoc}</p>
           </div>
-          <div ref={changedContainerRef} style={{ height: "100vh" }} />
+          <div id="changed-document-viewer" ref={changedContainerRef} className="h-lvh" />
         </div>
         <div className="col-span-2 sm:block border-1">
           <p className="p-3">Changes</p>
           <div>
-            <div className="p-3 border border-gray-400 border-1 rounded mx-auto mb-2 w-11/12">
-              <div className="flex justify-between">
-                <div className="text-gray-400 text-sm font-semibold uppercase">
-                  inserted
+            {Array.from(operationsMap).map(([key, value]) => (
+              <div key={key} className="p-1 border border-gray-400 border-1 rounded mx-auto mb-2 w-11/12">
+                <div className="flex justify-between p-1 pl-0">
+                  <div className="text-gray-400 text-xs">{value.insert && value.del ? "replaced" : value.insert ? "inserted" : "deleted"}</div>
+                  {plusMinusDisplayText(value)}
                 </div>
                 <div>
-                  <span className="bg-[#FFC9CB]">-1</span> |{" "}
-                  <span className="bg-[#C0D8EF]">+1</span>
+                  <p className="text-xs">
+                    <span className="bg-[#FFC9CB]">{value.deleteText}</span>
+                  </p>
+                  <p className="text-xs">
+                    <span className="bg-[#C0D8EF]">{value.insertText}</span>
+                  </p>
                 </div>
               </div>
-              <div>
-                <p>
-                  <span className="bg-[#FFC9CB]">inserted text</span>
-                </p>
-                <p>
-                  <span className="bg-[#C0D8EF]">deleted text</span>
-                </p>
-              </div>
-            </div>
+            ))}
           </div>
-          <TextChanges operations={operationsMap} />
         </div>
       </div>
     </div>
